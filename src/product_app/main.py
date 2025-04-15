9acfc198264abcc829ae1e7360618a64145e112b
marcelojgm <marcelogm2042@gmail.com>
2025-04-15 10:50:39 -0400

    Entries y button añadidos

--- src/product_app/main.py
+++ src/product_app/main.py
@@ -1,2 +1,20 @@
-print("Job Puto")
-print("Ah perro ya sirve")
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Registro de Productos")
+
+labelframe = tk.LabelFrame(window, text="Añadir Producto")
+labelframe.pack()
+
+tk.Label(labelframe, text="Nombre").pack()
+name = tk.Entry(labelframe)
+name.pack()
+
+tk.Label(labelframe, text="Precio").pack()
+price = tk.Entry(labelframe)
+price.pack()
+
+button = tk.Button(labelframe, text="Añadir Producto", command=lambda:print("Añadido"))
+button.pack()
+
+window.mainloop()
